98716623c9461cef85acf464acdfb3775c8f53e9
Luis Barrios <luis.barrios@alphas.technology>
2025-05-06 14:48:29 +0000

    merge all qa doctypes

---
+++ qp_maintenence/qp_maintenence/doctype/plantilla_recepcion_bienes/plantilla_recepcion_bienes.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Mentum Group and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Plantilla recepcion bienes', {
+	// refresh: function(frm) {
+
+	// }
+});
