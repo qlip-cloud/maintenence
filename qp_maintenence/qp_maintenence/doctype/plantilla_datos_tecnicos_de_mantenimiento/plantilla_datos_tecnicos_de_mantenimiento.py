d012d52113208e9d1a2cd6bd09d7caf9c4611d18
HendersonMentum <henderson.villegas@mentum.group>
2025-05-12 14:36:17 +0000

    add changes to all doctypes in this apps

--- qp_maintenence/qp_maintenence/doctype/plantilla_datos_tecnicos_de_mantenimiento/plantilla_datos_tecnicos_de_mantenimiento.py
+++ qp_maintenence/qp_maintenence/doctype/plantilla_datos_tecnicos_de_mantenimiento/plantilla_datos_tecnicos_de_mantenimiento.py
@@ -1,12 +1,8 @@
 # Copyright (c) 2025, Mentum Group and contributors
 # For license information, please see license.txt
 
-import frappe
+# import frappe
 from frappe.model.document import Document
-from six import iteritems, string_types
-from frappe.utils import today
 
-
-        
-class HojadeVidadelBien(Document):
+class Plantilladatostecnicosdemantenimiento(Document):
 	pass
