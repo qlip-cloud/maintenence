5b31a0884a3f82986ff23ec4e7389e679f02bc39
HendersonMentum <henderson.villegas@mentum.group>
2025-05-15 15:56:39 +0000

    add changes to equipo tecnico

--- qp_maintenence/qp_maintenence/doctype/equipo_tecnico/equipo_tecnico.js
+++ qp_maintenence/qp_maintenence/doctype/equipo_tecnico/equipo_tecnico.js
@@ -2,7 +2,26 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Equipo Tecnico', {
-	// refresh: function(frm) {
+	id_de_empleado:function(frm){
+		if(frm.doc.id_de_empleado){
+			frappe.db.get_list("Employee Education", { 
+				filters:{
+					'parenttype':'Employee', 'parentfield':"education", 'parent':frm.doc.id_de_empleado
+				},
+				fields:["*"],
+				order_by:"idx"
+			}).then((r) => {
+				if (r){
+					frm.set_value('formacion_academica', r[0].school_univ);
+				}
+				frm.refresh_field('formacion_academica')
+			});
+		}else{
+			frm.set_value('formacion_academica', '')
+		}
+
+		frm.refresh_field('formacion_academica')
+	}// refresh: function(frm) {
 
 	// }
 });
