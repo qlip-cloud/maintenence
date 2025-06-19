24ad4f16edb11c8afcf17b22e481a6e26ad86eee
HendersonMentum <henderson.villegas@mentum.group>
2025-06-19 16:29:00 +0000

    add changes

--- qp_maintenence/public/js/ma_transaction.js
+++ qp_maintenence/public/js/ma_transaction.js
@@ -1,3 +1,56 @@
+erpnext.TransactionController.prototype.setup_quality_inspection = function() {
+		if(!in_list(["Delivery Note", "Sales Invoice", "Purchase Receipt", "Purchase Invoice"], this.frm.doc.doctype)) {
+			return;
+		}
+
+		const me = this;
+
+		if(this.frm.doc.doctype in ["Purchase Receipt"]){
+			if (!this.frm.is_new()) {
+				this.frm.add_custom_button(__("Quality Inspection(s)"), () => {
+					me.make_quality_inspection();
+				}, __("Create"));
+				this.frm.page.set_inner_btn_group_as_primary(__('Create'));
+			}
+		}else{
+			if (!this.frm.is_new() && this.frm.doc.docstatus === 0) {
+				this.frm.add_custom_button(__("Quality Inspection(s)"), () => {
+					me.make_quality_inspection();
+				}, __("Create"));
+				this.frm.page.set_inner_btn_group_as_primary(__('Create'));
+			}
+		}
+
+		const inspection_type = in_list(["Purchase Receipt", "Purchase Invoice"], this.frm.doc.doctype)
+			? "Incoming" : "Outgoing";
+
+		let quality_inspection_field = this.frm.get_docfield("items", "quality_inspection");
+		quality_inspection_field.get_route_options_for_new_doc = function(row) {
+			if(me.frm.is_new()) return;
+			return {
+				"inspection_type": inspection_type,
+				"reference_type": me.frm.doc.doctype,
+				"reference_name": me.frm.doc.name,
+				"item_code": row.doc.item_code,
+				"description": row.doc.description,
+				"item_serial_no": row.doc.serial_no ? row.doc.serial_no.split("\n")[0] : null,
+				"batch_no": row.doc.batch_no
+			}
+		}
+
+		this.frm.set_query("quality_inspection", "items", function(doc, cdt, cdn) {
+			let d = locals[cdt][cdn];
+			return {
+				filters: {
+					docstatus: 1,
+					inspection_type: inspection_type,
+					reference_name: doc.name,
+					item_code: d.item_code
+				}
+			}
+		});
+}
+
 erpnext.TransactionController.prototype.make_quality_inspection = function() {
 
 		let data = [];
@@ -103,31 +156,30 @@ erpnext.TransactionController.prototype.make_quality_inspection = function() {
 			if (!item.quality_inspection) {
 				let dialog_items = dialog.fields_dict.items;
 
-				if(item.serial_no){
-					if(item.serial_no.split("\n").length > 1){
-						item.serial_no.split("\n").forEach(i => {
-							dialog_items.df.data.push({
-								"docname": item.name,
-								"item_code": item.item_code,
-								"item_name": item.item_name,
-								"qty": 1,
-								"description": item.description,
-								"serial_no": i,
-								"batch_no": item.batch_no
-							});
-						})
-					}else{
-							dialog_items.df.data.push({
+				if(item.serial_no && item.serial_no.split("\n").length > 1){
+					item.serial_no.split("\n").forEach(i => {
+						dialog_items.df.data.push({
 							"docname": item.name,
 							"item_code": item.item_code,
 							"item_name": item.item_name,
-							"qty": item.qty,
+							"qty": 1,
 							"description": item.description,
-							"serial_no": item.serial_no,
+							"serial_no": i,
 							"batch_no": item.batch_no
 						});
-					}
+					})
+				}else{
+					dialog_items.df.data.push({
+						"docname": item.name,
+						"item_code": item.item_code,
+						"item_name": item.item_name,
+						"qty": item.qty,
+						"description": item.description,
+						"serial_no": item.serial_no,
+						"batch_no": item.batch_no
+					});
 				}
+				
 				
 				dialog_items.grid.refresh();
 			}
