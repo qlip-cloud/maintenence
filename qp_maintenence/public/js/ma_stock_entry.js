db9c09cc048e5a77f8c2b1eec8a6fb21a416cefb
HendersonMentum <henderson.villegas@mentum.group>
2025-06-27 13:14:01 +0000

    add changes to taller y mantenimiento workspace

--- qp_maintenence/public/js/ma_stock_entry.js
+++ qp_maintenence/public/js/ma_stock_entry.js
@@ -59,6 +59,12 @@ frappe.ui.form.on('Stock Entry', {
                                                 };
                                                 frappe.set_route("List", 'Hoja de Vida del Bien');
                                             }
+                                        }else{
+                                            frappe.msgprint(
+                                                msg= "No existen productos configurados",
+                                                title='Mensaje',
+                                                raise_exception=FileNotFoundError
+                                            )
                                         }
                                     }
                                 });
