e8a7b36447d988fcab20c81405fda32c70089d43
HendersonMentum <114119448+HendersonMentum@users.noreply.github.com>
2025-10-10 17:52:09 -0400

    Revert "Qlip cloud staging"

--- qp_maintenence/public/js/ma_material_request.js
+++ qp_maintenence/public/js/ma_material_request.js
@@ -1,34 +1,4 @@
 frappe.ui.form.on("Material Request", {
-    onload:function(frm){
-		if(frm.doc.orden_de_trabajo != null){
-			frappe.db.get_list(
-				"Opportunity Item", 
-				{
-                    filters:{
-					    "parentfield":["in",["replacement_items", "consumable_items"]],
-					    "parent":frm.doc.orden_de_trabajo
-                    },
-                    fields:["*"]
-				}).then( (os) => {
-					if(Object.keys(os).length > 0){
-						os.forEach(function(d) {
-                            frm.add_child('items',{
-                                item_code:d.item_code,
-                                item_name:d.item_name,
-                                qty:d.qty,
-                                warehouse:d.warehouse,
-                                item_group:d.item_group,
-                                uom:d.uom,
-                                description:d.description,
-                                conversion_factor: 1
-                            })
-                        });
-                        frm.refresh_fields()
-					}
-				}
-			);
-		}
-	},
     project_mr:function(frm){
         if(frm.doc.project_mr != null){
             $.each(frm.doc.items, function(i, item){
