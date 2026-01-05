2d6087548758337c4aa50b49c6cfffa7cd4f05c8
HendersonMentum <henderson.villegas@mentum.group>
2026-01-05 21:52:12 +0000

    add changes to TASK-2025-19632

---
+++ qp_maintenence/qp_maintenence/doctype/profesiones/profesiones.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Mentum Group and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Profesiones', {
+	// refresh: function(frm) {
+
+	// }
+});
