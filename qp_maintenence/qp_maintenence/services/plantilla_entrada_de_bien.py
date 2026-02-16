136e8c6e03155f3ff68615f14891d90031ee5333
HendersonMentum <henderson.villegas@mentum.group>
2026-02-16 15:51:28 +0000

    add changes to TASK-2026-01970

---
+++ qp_maintenence/qp_maintenence/services/plantilla_entrada_de_bien.py
@@ -0,0 +1,35 @@
+
+
+import frappe
+from six import iteritems, string_types
+
+@frappe.whitelist()
+@frappe.validate_and_sanitize_search_inputs
+def handler(doctype, txt, searchfield, start, page_len, filters):
+		
+	r = frappe.db.sql("""
+		SELECT con.name
+		FROM `tabProject Template` con, `tabProductos Asociados PP` papp
+		WHERE papp.parent = con.name
+		AND papp.parenttype = 'Plantilla Entrada del Bien'
+		AND papp.item_code = '{item}'                  
+		LIMIT %(start)s, %(page_len)s
+		""".format(**{
+			'item': filters.get('item'),
+		}), {
+		'start': start,
+		'page_len': page_len
+	})
+
+	if len(r) == 0:
+
+		r = frappe.db.sql("""
+			SELECT con.name
+			FROM `tabProject Template` con
+			LIMIT %(start)s, %(page_len)s
+			""", {
+			'start': start,
+			'page_len': page_len
+		})
+
+	return r
