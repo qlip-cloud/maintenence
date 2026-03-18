83887b5e9a0da004acedbb3a62091a84368f99f4
HendersonMentum <henderson.villegas@mentum.group>
2026-03-18 15:15:10 +0000

    add changes to TASK-2026-03865

--- qp_maintenence/qp_maintenence/doctype/gestion_multiple_de_orden_de_servicio/gestion_multiple_de_orden_de_servicio.py
+++ qp_maintenence/qp_maintenence/doctype/gestion_multiple_de_orden_de_servicio/gestion_multiple_de_orden_de_servicio.py
@@ -117,6 +117,7 @@ def get_data(**args):
 									WHERE OS.cl_plantilla_de_mantenimiento = PT.name
 									AND OS.producto = HVB.item_code
 									AND OS.status = 'Completed'
+									AND OS.docstatus != 2
 									AND fecha_y_hora_finalización_os IS NOT NULL
 									ORDER by fecha_y_hora_finalización_os DESC
 									LIMIT 1) as fecha_ultimo_mantenimiento,
