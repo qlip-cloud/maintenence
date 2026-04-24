0a85e6dd49b68e30f8f566839dcb60d6d28bb289
HendersonMentum <henderson.villegas@mentum.group>
2026-04-24 19:34:03 +0000

    add changes to TASK-2026-05523

--- qp_maintenence/qp_maintenence/services/sales_invoice_from_maint.py
+++ qp_maintenence/qp_maintenence/services/sales_invoice_from_maint.py
@@ -17,8 +17,6 @@ def make_sales_invoice(source_name, target_doc=None, ignore_permissions=False):
 
 		target.tax_id  = frappe.db.get_value("Customer", target.customer, 'tax_id')
 
-		print(target)
-
 		set_missing_values(source, target)
 
 	def set_missing_values(source, target):
